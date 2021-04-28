ce1da7f3cdc1b83f23622abf8d6bcd7144322238
Juan Camilo <camo0402@gmail.com>
2021-04-28 04:49:15 -0500

    search bar

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,21 @@
 const Header = {
   render: () => `
         <div class="brand">
-                <a href="/#/">Logo</a>
-            </div>
+              <a href="/#/">Logo</a>
+              </div>
+              <div>
+              <div class="wrap">
+                  <div class="search">
+                      <input type="text" class="searchTerm" placeholder="What are you looking for?">
+                      <button type="submit" class="searchButton">
+                        <i class="fa fa-search"></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
             <div>
-                <a href="/#/cart">Carrito</a>
-            </div>
+              <a href="/#/cart">Carrito</a>
+          </div>
         `,
   after_render: () => {},
 };
